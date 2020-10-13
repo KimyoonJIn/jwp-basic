331e9f8ebf5a717ff3b351b89cca4fe5985fa024
KimyoonJIn <soosg0822@naver.com>
2020-10-13 17:16:44 +0900

    Solve the Q#4

--- src/main/java/next/controller/qna/CreateQuestionController.java
+++ src/main/java/next/controller/qna/CreateQuestionController.java
@@ -6,11 +6,14 @@ import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 import next.model.Answer;
 import next.model.Question;
+import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import static next.controller.UserSessionUtils.getUserFromSession;
 
 public class CreateQuestionController  extends AbstractController {
 
@@ -20,7 +23,19 @@ public class CreateQuestionController  extends AbstractController {
 
     @Override
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
-        Question question = new Question(req.getParameter("writer"), req.getParameter("title"),
+        // 로그인한 사용자만 질문이 가능하도록
+        if (getUserFromSession(req.getSession()) == null) {
+            return jspView("redirect:/users/loginForm");
+        }
+        User user = getUserFromSession(req.getSession());
+
+        // 질문할 때 글쓴이를 입력하지 않고 로그인한 사용자 정보를 가져와 글쓴이 이름으로 사용하도록 구현
+        String writer = req.getParameter("writer");
+        if (writer.equals("")){
+            writer = user.getUserId();
+        }
+
+        Question question = new Question(writer, req.getParameter("title"),
                 req.getParameter("contents"));
         Question savedQuestion = questionDao.insert(question);
         log.debug("question : {}", savedQuestion);
